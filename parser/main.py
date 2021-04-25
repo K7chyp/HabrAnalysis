f103437be0d7da7d2a7b905532b9ea0e85576c5d
NikitaVoronov <nekitos199@gmail.com>
2021-04-25 10:48:37 +0300

    Bug fixes + Lint it

--- parser/main.py
+++ parser/main.py
@@ -1,33 +1,29 @@
-from bs4 import BeautifulSoup
-from requests import get
 from functools import wraps
 from typing import final
-
+from bs4 import BeautifulSoup
+from requests import get
 
 @final
-class ProjectDecorators(object):
+class ProjectDecorators:
     @classmethod
     def result_processing(self, function_to_decorate):
         @wraps(function_to_decorate)
         def wrapper(self):
-            return list(set(function_to_decorate()))
+            return list(set(function_to_decorate(self)))
 
         return wrapper
 
 
 @final
-class HabrParser(object):
+class HabrParser:
     def __init__(self, url) -> None:
         self.url = url
         self.html = self.get_html(self.url)
         self.soup = BeautifulSoup(self.html, "lxml")
-        self.clear_page = self.get_page(self.html)
+        self.clear_page = self.soup.find("div")
 
     def get_html(self, url: str) -> str:
         return get(url).text
-
-    def get_page(self, html: str) -> str:
-        return self.soup.find("div")
 
     @ProjectDecorators.result_processing
     def get_articles_names(self) -> str:
@@ -35,7 +31,7 @@ class HabrParser(object):
         articles = ""
         for article in html_page:
             articles += article.text
-        return articles
+        return articles.split("\n")
 
     def get_stat(self) -> str:
         items = self.clear_page.find_all("span", {"class": "post-info__meta-item"})

015dddf74ed194d942941959d4092b6651482acf
NikitaVoronov <nekitos199@gmail.com>
2021-05-06 09:42:42 +0300

    update files

---
+++ Analysis/Preprocessing.py
@@ -0,0 +1,49 @@
+from random import randrange
+
+
+class ValuesPreprocessing:
+    def __init__(self, df):
+        self.df = df.copy()
+        self.df.columns = ["article_names", "raiting", "bookmarks", "viewers"]
+        self.delete_empty_values(column_name="article_names")
+        self.column_values_preprocessing()
+        self.apply_zero_values_changes()
+        self.df.drop("index", inplace=True, axis=1)
+
+    def delete_empty_values(self, column_name):
+        self.df = self.df[self.df[column_name].notna()].copy().reset_index().copy()
+
+    def column_values_preprocessing(self):
+        self.df["viewers"] = (
+            self.df["viewers"]
+            .apply(lambda x: float(x[:-1].replace(" ", "").replace(",", ".")) * 1000)
+            .copy()
+        )
+        self.df["viewers"] = self.df["viewers"].astype(int)
+        self.df["raiting"] = (
+            self.df["raiting"]
+            .apply(lambda x: x if type(x) == float else x.replace("&plus;", ""))
+            .copy()
+        )
+        self.df["raiting"] = self.df["raiting"].astype(float)
+        self.df["bookmarks"] = self.df["bookmarks"].astype(float)
+
+    def nan_to_zero(self, column_name):
+        self.df[column_name] = np.nan_to_num(self.df[column_name], 0.0)
+
+    def change_zero_value(self, column):
+        min_ = self.df[column].iloc[self.df[column].ne(0).idxmax()]
+        max_ = max(self.df[column])
+        for index in range(len(self.df[column])):
+            if self.df[column].iloc[index] == 0.0:
+                self.df.at[index, column] = randrange(min_, max_)
+
+    def apply_zero_values_changes(self):
+        for column in ("raiting", "bookmarks"):
+            self.nan_to_zero(column)
+            self.change_zero_value(column)
+
+
+class TextPreprocessing:
+    def __init__(self, df):
+        self.df = df.copy()

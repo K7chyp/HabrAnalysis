44bcda5528ca55008b70efbda7e6600a0881acf4
NikitaVoronov <nekitos199@gmail.com>
2021-05-06 15:55:25 +0300

    some fixes

--- Models/LinearModels.py
+++ Models/LinearModels.py
@@ -4,6 +4,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import SGDClassifier, LogisticRegressionCV
 from sklearn.metrics import accuracy_score, roc_auc_score, classification_report
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 PATH_TO_STOPWORDS = ""
 QUANTILES_DIVIDE = 5
